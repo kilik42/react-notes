8f42f6b53ebe22306f58c9c66e0f1a8495902ed9
marvin evins <maevins@cps.edu>
2019-05-13 15:28:23 -0500

    added notes components

---
+++ src/note/note.jsx
@@ -0,0 +1,20 @@
+import React, { Component} from 'react';
+import './Note.css';
+import PropTypes from 'prop-types';
+
+class Note extends Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props){
+    super(props);
+    this.message = "hello from the note component"
+  }
+  render(props) {
+    return (
+      <div>
+        <h1>hello from note component!</h1>
+      </div>
+    );
+  }
+}
+
+
+export default Note;
